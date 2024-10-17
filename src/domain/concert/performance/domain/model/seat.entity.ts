063ab9a0cf399d361475dddfc08a598fa8be2629
jae hyun <1994dbwogus@gmail.com>
2024-10-18 01:23:28 +0900

    feat: SeatEntity에 도메인 메서드 추가

--- src/domain/concert/performance/domain/model/seat.entity.ts
+++ src/domain/concert/performance/domain/model/seat.entity.ts
@@ -1,6 +1,6 @@
-import { Column, Entity, JoinColumn, VersionColumn } from 'typeorm';
+import { Column, Entity, JoinColumn } from 'typeorm';
 
-import { BaseEntity } from 'src/common';
+import { BaseEntity, ConflictStatusException } from 'src/common';
 import { SeatStatus } from './enum';
 
 @Entity('seat')
@@ -21,6 +21,36 @@ export class SeatEntity extends BaseEntity {
   })
   performanceId: number;
 
-  @VersionColumn()
-  version: number;
+  /* ================================ Domain Method ================================ */
+  /**
+   * 좌석이 예약 가능한 상태인지 확인합니다.
+   * @returns {boolean} 예약 가능 여부
+   */
+  isReservable(): boolean {
+    return this.status === SeatStatus.AVAILABLE;
+  }
+
+  /**
+   * 좌석을 예약합니다.
+   * @throws {ConflictStatusException} 좌석이 'Available' 상태가 아니라면 에러를 던집니다.
+   */
+  reserve(): this {
+    if (!this.isReservable()) {
+      throw new ConflictStatusException('좌석을 예약할 수 없는 상태 입니다.');
+    }
+    this.status = SeatStatus.RESERVED;
+    return this;
+  }
+
+  /**
+   * 좌석 예약을 확정합니다.
+   * @throws {ConflictStatusException} 좌석이 'Reserved' 상태가 아니라면 에러를 던집니다.
+   */
+  confirmReservation(): this {
+    if (this.status !== SeatStatus.RESERVED) {
+      throw new ConflictStatusException('좌석 "예약" 상태가 아닙니다.');
+    }
+    this.status = SeatStatus.BOOKED;
+    return this;
+  }
 }
