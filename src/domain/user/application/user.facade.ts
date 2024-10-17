1888851600fd6e154dabb0993de5b40ea16a2369
jae hyun <1994dbwogus@gmail.com>
2024-10-18 00:33:45 +0900

    feat: UserController, UserFacade 구현 완료

--- src/domain/user/application/user.facade.ts
+++ src/domain/user/application/user.facade.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import {
+  GetUserPointInfo,
+  UserService,
+  WriteUserPointCommand,
+} from '../domain';
 
 @Injectable()
-export class UserFacade {}
+export class UserFacade {
+  constructor(private readonly userService: UserService) {}
+
+  async getUserPoint(userId: number): Promise<GetUserPointInfo> {
+    const userPointInfo = await this.userService.getUserPoint(userId);
+    return userPointInfo;
+  }
+  async chargeUserPoint(
+    command: WriteUserPointCommand,
+  ): Promise<GetUserPointInfo> {
+    const userPointInfo = await this.userService.chargeUserPoint(command);
+    return userPointInfo;
+  }
+}
