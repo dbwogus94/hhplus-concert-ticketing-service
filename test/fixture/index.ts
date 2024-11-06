bcd6b955ab2c6563cb6648886f656d8c45879896
jae hyun <1994dbwogus@gmail.com>
2024-11-07 06:05:03 +0900

    refactor: 폴더 구조 변경

--- test/fixture/index.ts
+++ test/fixture/index.ts
@@ -1,11 +1,1 @@
-export * from './mock-entity-generator';
-
-export * from './concert-factory';
-export * from './performance-factory';
-export * from './seat-factory';
-export * from './point-factory';
-export * from './user-factory';
-export * from './payment-factory';
-export * from './reservation-factory';
-export * from './queue-factory';
-export * from './utils';
+export * from './factory';
