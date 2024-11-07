4ae46a286cf1074e140e47c9dac5f6f8a6cbe443
jae hyun <1994dbwogus@gmail.com>
2024-11-08 08:39:15 +0900

    refactor: app 종료시 RedisCacheStore가 연결하던 커넥션 제거 로직 추가

--- src/common/class/cache-store.ts
+++ src/common/class/cache-store.ts
@@ -1,7 +1,8 @@
 import { CacheStore } from '@nestjs/cache-manager';
+import { OnModuleDestroy } from '@nestjs/common';
 import { Redis } from 'ioredis';
 
-export type RedisType = Pick<Redis, 'get' | 'set' | 'setex' | 'del'>;
+export type RedisType = Pick<Redis, 'get' | 'set' | 'setex' | 'del' | 'quit'>;
 
 export abstract class BaseCacheStore implements CacheStore {
   abstract get<T>(key: string): Promise<T | undefined> | T | undefined;
@@ -9,12 +10,16 @@ export abstract class BaseCacheStore implements CacheStore {
   abstract del?(key: string): void | Promise<void>;
 }
 
-export class RedisCacheStore extends BaseCacheStore {
+export class RedisCacheStore extends BaseCacheStore implements OnModuleDestroy {
   constructor(private readonly redisClient: RedisType) {
     super();
   }
 
-  async get<T>(key: string): Promise<T | null> {
+  async onModuleDestroy() {
+    await this.redisClient.quit();
+  }
+
+  override async get<T>(key: string): Promise<T | null> {
     const value = await this.redisClient.get(key);
     return value ? JSON.parse(value) : null;
   }
@@ -25,7 +30,7 @@ export class RedisCacheStore extends BaseCacheStore {
    * @param value
    * @param ttl - `redisClient` ioredis를 사용하는 경우 초(seconds)단위만 사용가능하다.
    */
-  async set<T>(key: string, value: T, ttl?: number): Promise<void> {
+  override async set<T>(key: string, value: T, ttl?: number): Promise<void> {
     const stringValue = JSON.stringify(value);
 
     if (ttl) {
@@ -35,7 +40,7 @@ export class RedisCacheStore extends BaseCacheStore {
     }
   }
 
-  async del(key: string): Promise<void> {
+  override async del(key: string): Promise<void> {
     await this.redisClient.del(key);
   }
 }
