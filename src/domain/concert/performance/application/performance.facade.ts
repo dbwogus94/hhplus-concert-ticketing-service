61965743069cac377a2c20992da653b5d45e0f56
jae hyun <1994dbwogus@gmail.com>
2024-11-15 13:01:12 +0900

    refactor: 커링 트랜잭션 제거 코드 반영

--- src/domain/concert/performance/application/performance.facade.ts
+++ src/domain/concert/performance/application/performance.facade.ts
@@ -23,10 +23,10 @@ export class PerformanceFacade {
   }
 
   async reserveSeat(seatId: number) {
-    await this.performanceService.reserveSeat(seatId)();
+    await this.performanceService.reserveSeat(seatId);
   }
 
   async bookingSeat(seatId: number) {
-    await this.performanceService.bookingSeat(seatId)();
+    await this.performanceService.bookingSeat(seatId);
   }
 }
