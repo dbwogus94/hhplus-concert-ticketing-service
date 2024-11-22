008659dc39bee693169d0a2d96b2ce7d6fe68247
jae hyun <1994dbwogus@gmail.com>
2024-11-22 09:25:43 +0900

    feat: PerformanceConsumer 신규 정의

---
+++ src/domain/concert/performance/presentation/consumer/performance.consumer.ts
@@ -0,0 +1,20 @@
+import { Controller, Inject } from '@nestjs/common';
+import { ClientKafka, MessagePattern } from '@nestjs/microservices';
+
+import { KAFKA_CLIENT_NAME } from 'src/common';
+import { ConsumRequestReservation } from './dto';
+
+@Controller('performance-consumer')
+export class PerformanceConsumer {
+  constructor(
+    @Inject(KAFKA_CLIENT_NAME)
+    private readonly kafkaClient: ClientKafka,
+  ) {}
+
+  @MessagePattern('reservation.request.topic')
+  async handleRequestReservation(data: ConsumRequestReservation) {
+    const { payload } = data;
+    console.log('handleRequestReservation: ', payload);
+    return data;
+  }
+}
